3e7af47deeade41de3a7edd9852e1f6df372a857
Gyudonn <xjirmpmxjirmpm@gmail.com>
2021-06-27 18:45:08 +0900

    python update

--- products.py
+++ products.py
@@ -1,16 +1,25 @@
 products = []
+products_check=[]
 
 while True:
 	name = input('input the product name: ')
 	if name == 'q':
+		with open('product.csv', 'w', encoding = 'utf-8') as f:
+			f.write('Good,Price\n')
+			for product in products:
+				f.write(product[0] + ',' + str(product[1]) + '\n')
 		break
 	price = input('input the product price: ')
+	price = int(price)
 	product = [name, price]
 	products.append(product)
 
 for product in products:
-	print('The price of ' + product[0] + ' is ' + product[1])
+	print('The price of ' + product[0] + ' is ' + str(product[1]))
 
-with open('product.txt', 'w') as f:
-	for product in products:
-		f.write(product[0] + ',' + product[1] + '\n')
+with open('product.csv' , 'r', encoding = 'utf-8') as file:
+			for line in file:
+				name, price = line.strip().split(',')
+				product = [name, price]
+				products_check.append(product)
+print(products_check)
